1cf93b8063efe499110b43e34ade0e4009e9709b
Ubuntu <ubuntu@ip-172-31-39-51.us-east-2.compute.internal>
2020-01-02 22:50:17 +0000

    Update

--- src/app/controllers/convidadosController.js
+++ src/app/controllers/convidadosController.js
@@ -3,9 +3,19 @@
 class ConvidadosController {
 
     async ordenate(req, res) {
+	    
+
+	    var i;
+
+	    const {convidados} = req.body
+
+	    let convidadosLista = convidados.replace(/_/g, ' ').split(',');
 
 
-        return res.json({ ok: "true" }).status(200);
+	    const ordenados = convidadosLista.sort()
+
+
+        return res.json({ ordenados  }).status(200);
     }
 
 }
