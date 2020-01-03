e1c1a4cff918874875e012192c53d8fac940d002
Joao pedro furini <jp.furini@hotmail.com>
2020-01-03 00:40:32 -0200

    Add git configuration

--- src/app/controllers/convidadosController.js
+++ src/app/controllers/convidadosController.js
@@ -3,19 +3,19 @@
 class ConvidadosController {
 
     async ordenate(req, res) {
-	    
-
-	    var i;
-
-	    const {convidados} = req.body
-
-	    let convidadosLista = convidados.replace(/_/g, ' ').split(',');
 
 
-	    const ordenados = convidadosLista.sort()
+        var i;
+
+        const { convidados } = req.body
+
+        let convidadosLista = convidados.replace(/_/g, ' ').split(',');
 
 
-        return res.json({ ordenados  }).status(200);
+        const ordenados = convidadosLista.sort()
+
+
+        return res.json({ ordenados }).status(200);
     }
 
 }
